977a1b30b5aedc8967e3fddfda485e1b86012ebb
Turismo Racing <email@turismoracing.com>
2023-09-07 17:43:20 -0500

    Work commit, working on the add friend component functionality

--- Projects/eat-n-split/src/App.js
+++ Projects/eat-n-split/src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const initialFriends = [{
     id: 118836, name: "Clark", image: "https://i.pravatar.cc/48?u=118836", balance: -7,
 }, {
@@ -7,26 +9,30 @@ const initialFriends = [{
 },];
 
 export default function App() {
+    const [addFriend, setAddFriend] = useState(false);
+
     return (<div className="app">
         <div className="sidebar">
             <FriendsList/>
-            <FormAddFriend/>
-            <Button>Add friend</Button>
+            {addFriend ? <FormAddFriend/> : ""}
+            <Button handleOnClick={() => setAddFriend(!addFriend)}>{addFriend ? "Close" : "Add friend"}</Button>
             <FormSplitBill />
         </div>
     </div>)
 }
 
-function Button({children}) {
-    return <button className="button">{children}</button>
+function Button({children, handleOnClick}) {
+    return <button className="button" onClick={handleOnClick}>{children}</button>
 }
 
 function FriendsList() {
-    const friends = initialFriends;
-    return <ul>{friends.map((friend) => <Friend key={friend.id} friend={friend}/>)}</ul>
+    const [friendsArray, setFriendsArray] = useState([]);
+    //const friends = initialFriends;
+    return <ul>{friendsArray.map((friend) => <Friend key={friend.id} friend={friend}/>)}</ul>
 }
 
 function Friend({friend}) {
+
     return <li><img src={friend.image} alt={friend.name}/><h3>{friend.name}</h3>{friend.balance < 0 ? (
         <p className="red">You owe {friend.name} ${Math.abs(friend.balance)}</p>) : friend.balance > 0 ?
         <p className="green">{friend.name} owes you ${Math.abs(friend.balance)}</p> :
@@ -36,12 +42,23 @@ function Friend({friend}) {
 }
 
 function FormAddFriend() {
+    const [friendName, setFriendName] = useState("");
+    const [imageURL, setImageURL] = useState("");
+
+    function handleSetFriendName(e) {
+        setFriendName(e.target.value);
+    }
+
+    function handleImageURL(e) {
+        setImageURL(e.target.value);
+    }
+
     return <form action="" className="form-add-friend">
         <label htmlFor="name">👫 Friend name</label>
-        <input type="text" id="name"/>
+        <input type="text" id="name" value={friendName} onChange={handleSetFriendName}/>
 
         <label htmlFor="image">🌆 Image URL</label>
-        <input type="url" id="image"/>
+        <input type="url" id="image" value={imageURL} onChange={handleImageURL}/>
 
         <Button>Add</Button>
     </form>
@@ -51,5 +68,21 @@ function FormSplitBill() {
     return (<form action="" className="form-split-bill">
         <h2>Split a bill with Sarah</h2>
 
+        <label>💰 Bill value</label>
+        <input type="number" />        
+
+        <label>🧍 Your expense</label>
+        <input type="number"/>
+
+        <label>🧑‍🤝‍🧑 Sarah's expense:</label>
+        <input type="number" disabled/>
+
+        <label>🤑 Who is paying the bill?</label>
+        <select>
+            <option value="user">You</option>
+            <option value="friend">Other</option>
+        </select>
+
+        <Button>Split bill</Button>
     </form>)
 }
