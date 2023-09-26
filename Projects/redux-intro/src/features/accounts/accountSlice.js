86a952a820ed6d62ef1967c7ed0d6f1f6bbdc633
Turismo Racing <email@turismoracing.com>
2023-09-26 17:44:38 -0500

    Learning more about Redux, work

--- Projects/redux-intro/src/features/accounts/accountSlice.js
+++ Projects/redux-intro/src/features/accounts/accountSlice.js
@@ -1,9 +1,9 @@
-
 const initialStateAccount = {
     balance: 0,
     loan: 0,
     loanPurpose: "",
-}
+    isLoading: false,
+};
 
 export default function accountReducer(state = initialStateAccount, action) {
     switch (action.type) {
@@ -11,12 +11,13 @@ export default function accountReducer(state = initialStateAccount, action) {
             return {
                 ...state,
                 balance: state.balance + action.payload,
-            }
+                isLoading: false,
+            };
         case "accounts/withdraw":
             return {
                 ...state,
                 balance: state.balance - action.payload,
-            }
+            };
         case "accounts/requestLoan":
             if (state.loan > 0) return state;
             return {
@@ -24,34 +25,53 @@ export default function accountReducer(state = initialStateAccount, action) {
                 loan: action.payload.amount,
                 loanPurpose: action.payload.purpose,
                 balance: state.balance + action.payload.amount,
-            }
+            };
         case "accounts/payLoan":
             return {
                 ...state,
                 loan: 0,
                 loanPurpose: "",
                 balance: state.balance - state.loan,
-            }
+            };
+        case "account/convertingCurrency":
+            return {
+                ...state,
+                isLoading: true,
+            };
         default:
             return state;
     }
 }
 
-export function deposit(amount) {
-    return {type: "accounts/deposit", payload: amount};
+export function deposit(amount, currency) {
+    if (currency === "USD") {
+        return { type: "accounts/deposit", payload: amount };
+    }
+
+    return async function (dispatch, getState) {
+        dispatch({ type: "account/convertingCurrency" });
+
+        const res = await fetch(
+            `https://api.frankfurter.app/latest?amount=${amount}&from=${currency}&to=USD`
+        );
+        const data = await res.json();
+        const converted = data.rates.USD;
+
+        dispatch({ type: "accounts/deposit", payload: converted });
+    };
 }
 
 export function withdraw(amount) {
-    return {type: "accounts/withdraw", payload: amount};
+    return { type: "accounts/withdraw", payload: amount };
 }
 
 export function requestLoan(amount, purpose) {
     return {
         type: "accounts/requestLoan",
-        payload: {amount, purpose},
+        payload: { amount, purpose },
     };
 }
 
 export function payLoan() {
-    return {type: "accounts/payLoan"};
+    return { type: "accounts/payLoan" };
 }
