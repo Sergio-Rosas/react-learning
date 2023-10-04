e20d6b03fcf173d3910313976b1752aa87cd6025
Sergio <gunners.src@hotmail.com>
2023-10-03 22:07:57 -0500

    Learning React Query

--- Projects/the-wild-oasis/src/App.jsx
+++ Projects/the-wild-oasis/src/App.jsx
@@ -1,5 +1,9 @@
 import GlobalStyles from "./styles/GlobalStyles.js";
 import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import {QueryClient} from "@tanstack/react-query";
+import {QueryClientProvider} from "@tanstack/react-query";
+import {ReactQueryDevtools} from "@tanstack/react-query-devtools";
+
 import Dashboard from "./pages/Dashboard.jsx";
 import Bookings from "./pages/Bookings.jsx";
 import Cabins from "./pages/Cabins.jsx";
@@ -10,32 +14,40 @@ import Login from "./pages/Login.jsx";
 import PageNotFound from "./pages/PageNotFound.jsx";
 import AppLayout from "./ui/AppLayout.jsx";
 
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            //staleTime: 60 * 1000,
+            staleTime: 0,
+        },
+    },
+});
+
 function App() {
     return (
-        <>
-            <GlobalStyles/>
-            <BrowserRouter>
-                <Routes>
-                    <Route element={<AppLayout/>}>
-                        <Route index element={<Navigate replace to="dashboard"/>}/>
-                        <Route path="dashboard" element={<Dashboard/>}/>
-                        <Route path="bookings" element={<Bookings/>}/>
-                        <Route path="cabins" element={<Cabins/>}/>
-                        <Route path="users" element={<Users/>}/>
-                        <Route path="settings" element={<Settings/>}/>
-                        <Route path="account" element={<Account/>}/>
-                    </Route>
-                    <Route path="login" element={<Login/>}/>
-                    <Route path="*" element={<PageNotFound/>}/>
-                </Routes>
-            </BrowserRouter>
-        </>
+            <QueryClientProvider client={queryClient}>
+                <ReactQueryDevtools initialIsOpen={false}/>
+                <GlobalStyles/>
+                <BrowserRouter>
+                    <Routes>
+                        <Route element={<AppLayout/>}>
+                            <Route index element={<Navigate replace to="dashboard"/>}/>
+                            <Route path="dashboard" element={<Dashboard/>}/>
+                            <Route path="bookings" element={<Bookings/>}/>
+                            <Route path="cabins" element={<Cabins/>}/>
+                            <Route path="users" element={<Users/>}/>
+                            <Route path="settings" element={<Settings/>}/>
+                            <Route path="account" element={<Account/>}/>
+                        </Route>
+                        <Route path="login" element={<Login/>}/>
+                        <Route path="*" element={<PageNotFound/>}/>
+                    </Routes>
+                </BrowserRouter>
+            </QueryClientProvider>
     )
 }
 
 export default App;
-
-
 
 
 /*
