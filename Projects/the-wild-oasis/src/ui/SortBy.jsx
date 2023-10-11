722fa09e7dbf7b0391289cd7b021ccaf3a742cab
Sergio <gunners.src@hotmail.com>
2023-10-10 22:41:04 -0500

    More about the app

--- Projects/the-wild-oasis/src/ui/SortBy.jsx
+++ Projects/the-wild-oasis/src/ui/SortBy.jsx
@@ -1,8 +1,17 @@
+import Select from "./Select.jsx";
+import {useSearchParams} from "react-router-dom";
+
 function SortBy({options}) {
+    const [searchParams, setSearchParams] = useSearchParams();
+    const sortBy = searchParams.get("sortBy") || "";
+
+    function handleChange(e) {
+        searchParams.set("sortBy", e.target.value);
+        setSearchParams(searchParams);
+    }
+
     return (
-        <div>
-            Sort
-        </div>
+        <Select options={options} type="white" onChange={handleChange} value={sortBy}/>
     )
 }
 
