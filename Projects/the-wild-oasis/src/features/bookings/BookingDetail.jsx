3e47c381c340cc9813dd901781ec7fe7164edf9f
Turismo Racing <email@turismoracing.com>
2023-10-12 17:56:33 -0500

    Finished the check-in and check-out, work

--- Projects/the-wild-oasis/src/features/bookings/BookingDetail.jsx
+++ Projects/the-wild-oasis/src/features/bookings/BookingDetail.jsx
@@ -9,49 +9,72 @@ import Button from "../../ui/Button";
 import ButtonText from "../../ui/ButtonText";
 
 import { useMoveBack } from "../../hooks/useMoveBack";
-import {useBooking} from "./useBooking.js";
+import { useBooking } from "./useBooking.js";
 import Spinner from "../../ui/Spinner.jsx";
+import { useNavigate } from "react-router-dom";
+import { useCheckout } from "../check-in-out/useCheckout";
+import { HiArrowUpOnSquare } from "react-icons/hi2";
 
 const HeadingGroup = styled.div`
-  display: flex;
-  gap: 2.4rem;
-  align-items: center;
+    display: flex;
+    gap: 2.4rem;
+    align-items: center;
 `;
 
 function BookingDetail() {
-  const {booking, isLoading} = useBooking();
+    const { booking, isLoading } = useBooking();
+    const navigate = useNavigate();
+    const { checkout, isCheckingOut } = useCheckout();
 
-  const moveBack = useMoveBack();
+    const moveBack = useMoveBack();
 
-  const statusToTagName = {
-    unconfirmed: "blue",
-    "checked-in": "green",
-    "checked-out": "silver",
-  };
+    const statusToTagName = {
+        unconfirmed: "blue",
+        "checked-in": "green",
+        "checked-out": "silver",
+    };
 
-  if (isLoading) return <Spinner/>;
+    if (isLoading) return <Spinner />;
 
-  const {status, id: bookingId} = booking;
+    const { status, id: bookingId } = booking;
 
-  return (
-    <>
-      <Row type="horizontal">
-        <HeadingGroup>
-          <Heading as="h1">Booking #{bookingId}</Heading>
-          <Tag type={statusToTagName[status]}>{status.replace("-", " ")}</Tag>
-        </HeadingGroup>
-        <ButtonText onClick={moveBack}>&larr; Back</ButtonText>
-      </Row>
+    return (
+        <>
+            <Row type="horizontal">
+                <HeadingGroup>
+                    <Heading as="h1">Booking #{bookingId}</Heading>
+                    <Tag type={statusToTagName[status]}>
+                        {status.replace("-", " ")}
+                    </Tag>
+                </HeadingGroup>
+                <ButtonText onClick={moveBack}>&larr; Back</ButtonText>
+            </Row>
 
-      <BookingDataBox booking={booking} />
+            <BookingDataBox booking={booking} />
 
-      <ButtonGroup>
-        <Button variation="secondary" onClick={moveBack}>
-          Back
-        </Button>
-      </ButtonGroup>
-    </>
-  );
+            <ButtonGroup>
+                {status === "unconfirmed" && (
+                    <Button onClick={() => navigate(`/checkin/${bookingId}`)}>
+                        Check in
+                    </Button>
+                )}
+
+                {status === "checked-in" && (
+                    <Button
+                        icon={<HiArrowUpOnSquare />}
+                        onClick={() => checkout(bookingId)}
+                        disabled={isCheckingOut}
+                    >
+                        Check out
+                    </Button>
+                )}
+
+                <Button variation="secondary" onClick={moveBack}>
+                    Back
+                </Button>
+            </ButtonGroup>
+        </>
+    );
 }
 
 export default BookingDetail;
